c62f2d237a947a3133ec75f455f15daf84a2d665
dxsup <idxwang@outlook.com>
2019-01-18 15:41:36 +0800

    Initial Commit

---
+++ DXLaunchAdDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DXLaunchAdDemo
+//
+//  Created by Daxin on 2019/1/18.
+//  Copyright © 2019 dxsup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
